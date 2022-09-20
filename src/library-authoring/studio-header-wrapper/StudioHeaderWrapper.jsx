38d77afc1f706dfdc405d886bd00b23f36569aa4
Brian Smith <bsmith@tcril.org>
2022-09-20 17:45:55 -0400

    move settings dropdown into header, update contenttitle link

--- src/library-authoring/studio-header-wrapper/StudioHeaderWrapper.jsx
+++ src/library-authoring/studio-header-wrapper/StudioHeaderWrapper.jsx
@@ -6,8 +6,17 @@ import {
   fetchLibraryDetail,
 } from '../author-library/data';
 import { injectIntl } from '@edx/frontend-platform/i18n';
+import {
+  ActionRow,
+  Dropdown
+} from '@edx/paragon';
+import libraryDetailMessages from '../author-library/messages';
+import { Link } from 'react-router-dom';
+import {
+  ROUTES,
+} from '../common';
 
-const StudioHeaderWrapperBase = ({...props}) => {
+const StudioHeaderWrapperBase = ({intl, ...props}) => {
   // loadingStatus will only ever be 'loaded' on pages
   // where we have library details, so we can use that to
   // determine if we want to render the ContentTitleBlock or not
@@ -16,13 +25,25 @@ const StudioHeaderWrapperBase = ({...props}) => {
   const actionRowContent = (
     <>
       {(loadingStatus === 'loaded') ? 
-          <ContentTitleBlock 
-            title={library.title}
-            subtitle={library.org}
-            destination='#'/> :
-          <></>
+          <>
+            <ContentTitleBlock 
+              title={library.title}
+              subtitle={library.org}
+              destination={ROUTES.Detail.HOME_SLUG(library.id)}/>
+            <ActionRow.Spacer />
+            <Dropdown>
+              <Dropdown.Toggle id="library-header-menu-dropdown">
+                {intl.formatMessage(libraryDetailMessages['library.detail.settings.menu'])}
+                {/* <Icon className="fa fa-caret-down pl-3" alt="" /> */}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item as={Link} to={ROUTES.Detail.EDIT_SLUG(library.id)}>{intl.formatMessage(libraryDetailMessages['library.detail.settings.details'])}</Dropdown.Item>
+                <Dropdown.Item as={Link} to={ROUTES.Detail.ACCESS_SLUG(library.id)}>{intl.formatMessage(libraryDetailMessages['library.detail.settings.access'])}</Dropdown.Item>
+                <Dropdown.Item as={Link} to={ROUTES.Detail.IMPORT_SLUG(library.id)}>{intl.formatMessage(libraryDetailMessages['library.detail.settings.import'])}</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </> : <></>
       }
-      <div>BLARG</div>
     </>
   );
 
