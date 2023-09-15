54183805fc7bceceeb07b782b72f994912db744c
KristinAoki <kaoki@2u.com>
2023-09-15 09:21:50 -0400

    fix: styles of license details section

--- src/library-authoring/common/LicenseField.jsx
+++ src/library-authoring/common/LicenseField.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Button,
+  ButtonGroup,
   Col,
   Form,
   Row,
@@ -36,22 +37,15 @@ export const LicenceFieldBase = (
     <label htmlFor={name}>
       {intl.formatMessage(messages['library.common.fields.license.label'])}
     </label>
-    <Row className="flex-row">
-      <Col>
-        <Button name={name} className="text-uppercase mx-1" variant={reservedVariant} onClick={() => updateValue('')}>
+    <Row className="m-0">
+      <ButtonGroup>
+        <Button name={name} className="text-uppercase" variant={reservedVariant} onClick={() => updateValue('')}>
           {intl.formatMessage(messages['library.common.license.none'])}
         </Button>
-      </Col>
-      <Col>
-        <Button name={name} className="text-uppercase mx-1" variant={commonsVariant} onClick={() => updateValue(spec)}>
+        <Button name={name} className="text-uppercase" variant={commonsVariant} onClick={() => updateValue(spec)}>
           {intl.formatMessage(messages['library.common.license.cc'])}
         </Button>
-        <p className="small">
-          <a target="_blank" rel="noopener noreferrer" href="https://creativecommons.org/about">
-            {intl.formatMessage(messages['library.common.fields.license.cc.learn_more'])}
-          </a>
-        </p>
-      </Col>
+      </ButtonGroup>
     </Row>
     {value && (
       <>
@@ -182,9 +176,9 @@ export const LicenseFieldContainerBase = ({ value, updateValue, name }) => {
   let reservedVariant;
   if (value) {
     commonVariant = 'primary';
-    reservedVariant = 'light';
+    reservedVariant = 'outline-primary';
   } else {
-    commonVariant = 'light';
+    commonVariant = 'outline-primary';
     reservedVariant = 'primary';
   }
 
