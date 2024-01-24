c25fea3cce6eb3273287bf948cbbd70e83858289
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2024-01-24 15:46:12 +0500

    refactor: updated jest config & setupTest files

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ const { createConfig } = require('@edx/frontend-build');
 module.exports = createConfig('jest', {
   setupFilesAfterEnv: [
     '<rootDir>/src/setupTest.js',
+    '<rootDir>/src/setupTestEnv.js',
   ],
   collectCoverageFrom: [
     "**/*.{js,jsx}",
